25ff09a757ef8c4500f2fa8f1db7910cffc2e021
Geunyeong Yu <geunyeong0211@gmail.com>
2024-11-10 02:35:18 +0900

    Initial settings

---
+++ src/main/java/org/example/springbootboard/SpringBootBoardApplication.java
@@ -0,0 +1,11 @@
+package org.example.springbootboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBoardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class,args);
+    }
+}
